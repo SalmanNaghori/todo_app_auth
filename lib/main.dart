a35d31a9e08fe75b369fca08a12a81b05935d15c
SalmanNaghori <69813706+SalmanNaghori@users.noreply.github.com>
2023-10-22 23:44:32 +0530

    today 22's updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_app_auth/screen/home_screen.dart';
+import 'package:todo_app_auth/theme/colors/light_colors.dart';
 
 void main() async {
   //initalize firebase
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: LightColors.lightYellow, // navigation bar color
+    statusBarColor: Color(0xffffb969), // status bar color
+  ));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -18,10 +24,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: LightColors.darkBlue,
+              displayColor: LightColors.darkBlue,
+              fontFamily: 'Poppins',
+            ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
